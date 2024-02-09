de61d5bc60d6bd2435c479d2340d6d51b66aea04
mundoc <64046860+mundoc@users.noreply.github.com>
2024-02-09 19:01:08 +0100

    Update merged_predict_next_match_automated.py

--- merged_predict_next_match_automated.py
+++ merged_predict_next_match_automated.py
@@ -620,7 +620,7 @@ def plot_donut_chart(home_percentage, draw_percentage, away_percentage):
     # Create the donut chart
     fig = go.Figure(data=[go.Pie(labels=outcome_labels,
                                  values=percentages,
-                                 hole=.4,
+                                 hole=.3,
                                  marker_colors=colors,
                                  textinfo='text+percent',
                                  textposition='outside',
@@ -653,7 +653,7 @@ for index, row in df_nn_predictions[:10].iterrows():
     st.write(f"**{home_team}** vs **{away_team}**")
     
     # Display the chart in the Streamlit app
-    st.plotly_chart(fig, use_container_width=True, width=300, height=150)
+    st.plotly_chart(fig, use_container_width=True, width=100, height=50)
 
 st.title('EPL Predictions')
 
